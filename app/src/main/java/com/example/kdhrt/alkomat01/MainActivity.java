ec51ca28ed50429623ee2cdc1f1cded6c0cdd02b
Maciej <petrykowski99@gmail.com>
2019-05-12 21:11:56 +0200

    Alkomat zapisywanie

--- app/src/main/java/com/example/kdhrt/alkomat01/MainActivity.java
+++ app/src/main/java/com/example/kdhrt/alkomat01/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kdhrt.alkomat01;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -13,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
+        }
 
 
     }
--- app/src/main/java/com/example/kdhrt/alkomat01/app2.java
+++ app/src/main/java/com/example/kdhrt/alkomat01/app2.java
@@ -1,16 +1,33 @@
 package com.example.kdhrt.alkomat01;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class app2 extends AppCompatActivity {
+
+    String wynik;
+    String path= "sdcard/Android/data/Alkomat";
+    private final int MEMORY_ACESS=5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +39,26 @@ public class app2 extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         RadioButton m = findViewById(R.id.mezczyzna);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(app2.this, Manifest.permission.WRITE_EXTERNAL_STORAGE));
+        else
+            ActivityCompat.requestPermissions(app2.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MEMORY_ACESS);
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MEMORY_ACESS:
+                if(grantResults.length>0&&grantResults[0]== PackageManager.PERMISSION_GRANTED){
+
+                }else
+                    Toast.makeText(getApplicationContext(),"brak pozwolenia",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void oblicz(View view) {
@@ -48,6 +83,40 @@ public class app2 extends AppCompatActivity {
         //String nazwa=String.valueOf(nazwaS.getText());
 
         Alkomat alkomat=new Alkomat();
-        promil.setText(""+(alkomat.policzP(procent,ml,ilosc,plec,masa))+"‰o");
+
+        wynik=""+(alkomat.policzP(procent,ml,ilosc,plec,masa))+"‰o";
+        promil.setText(wynik);
+    }
+
+    public void zapisz(View view) {
+        if(wynik!=null) {
+            createDir();
+            createFile();
+        }
+    }
+
+    public void createDir(){
+        File file=new File(path);
+        if(!file.exists()){
+            file.mkdir();
+
+        }
+    }
+
+    public void createFile() {
+        File file=new File(path+"/data.txt");
+        if(!file.exists()){
+            try {
+                FileOutputStream fos= new FileOutputStream(file);
+                OutputStreamWriter writer=new OutputStreamWriter(fos);
+                writer.append(wynik);
+                writer.close();
+                fos.close();
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
     }
 }
